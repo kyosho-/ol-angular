7650f8727716123d99e3ea15fc681954724587ee
kyosho- <info@kyosho.net>
2019-09-12 20:21:32 +0900

    add introduction.

--- projects/ol-angular-example/src/app/content/introduction/introduction.component.ts
+++ projects/ol-angular-example/src/app/content/introduction/introduction.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Content } from '../../common/content';
+import { ExampleService } from '../example/example.service';
 
 @Component({
   selector: 'app-introduction',
@@ -7,9 +10,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IntroductionComponent implements OnInit {
 
-  constructor() { }
+  contents$: Observable<Content[]>;
+
+  constructor(private exampleService: ExampleService) { }
 
   ngOnInit() {
+    this.contents$ = this.exampleService.getExamples();
   }
-
 }
